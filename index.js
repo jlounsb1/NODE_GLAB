d45d4ff570f9ee57ae37d4ac7c872bd74d047b89
jlounsb1 <jefflounsbury@gmail.com>
2024-06-27 15:24:34 -0400

    added different req options in if statements

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello World, Merps")
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode =200;
+    res.setHeader('Content-Type', 'text/html');
+    if(req.url ==="/") {
+        res.write('<h1 style="color:red">Hello World!</h1>');
+        res.write('<p>I wonder what else we can send...</p>');
+    } else if (req.url =="/about") {
+        res.write('<h1 style="color:green">I mean, I am pretty boring...</h1>');
+        res.write('<p>I could give you a three hour lecture on how to craft a bow in Path of Exile if you want to pass the time.</p>');
+    } else {
+        res.statusCode = 404;
+        res.write("Error: 404 Not Found. You done goofed!!");
+    }
+    res.end();
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
